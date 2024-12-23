7d8fd9db2c9c307538b700380cedee47c3aadb84
hdemiray <dhalilullah@gmail.com>
2024-12-23 17:34:36 +0300

    firebase tools added

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,108 @@
+import 'package:chatapp/components/my_button.dart';
+import 'package:chatapp/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  final TextEditingController _confirmPwController = TextEditingController();
+
+  //to go to login page
+  final void Function()? onTap;
+
+  RegisterPage({super.key, required this.onTap});
+
+  //login function
+  void register() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            const SizedBox(height: 50),
+
+            //welcome back message
+            Text(
+              "Let's create your account!",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+
+            const SizedBox(height: 50),
+
+            //email textfield
+            MyTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
+
+            const SizedBox(height: 10),
+
+            //pw textfield
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _pwController,
+            ),
+
+            const SizedBox(height: 10),
+
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _confirmPwController,
+            ),
+
+            const SizedBox(height: 25),
+
+            //login button
+            MyButton(
+              text: "Register",
+              onTap: register,
+            ),
+
+            const SizedBox(height: 25),
+
+            //register now
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already have an account? ",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Text(
+                    "Login now",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
