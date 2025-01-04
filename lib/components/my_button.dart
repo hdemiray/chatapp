4cd88283225219e47b5c4b7308950bc82c825a20
hdemiray <dhalilullah@gmail.com>
2025-01-04 18:04:13 +0300

    log out added android fixed

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -19,7 +19,7 @@ class MyButton extends StatelessWidget {
           color: Theme.of(context).colorScheme.secondary,
           borderRadius: BorderRadius.circular(8),
         ),
-        padding: EdgeInsets.all(25),
+        padding: EdgeInsets.all(15),
         margin: EdgeInsets.all(25),
         child: Center(
           child: Text(text),
